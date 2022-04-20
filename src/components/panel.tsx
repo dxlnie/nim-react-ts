7341d165e2b8f528209f24a1854ffa3a37c98d7c
dxlnie <dxlnie@163.com>
2022-04-20 22:00:39 +0800

    控件

--- src/components/panel.tsx
+++ src/components/panel.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import 'panel.css';
+import toothpick from './toothpick';
 
 // interface Props{
 //   selected:boolean
 // }
+function createToothPickRow(count:number){
+  const row=[]
+  for (let i=0;i<count;i++){
+    row.push(toothpick)
+  }
+  return row
+}
 
 function panel() {
+
+  const firstRow = createToothPickRow(3)
+  const secondRow = createToothPickRow(5)
+  const thirdRow = createToothPickRow(7)
+
+
   return (
     <div className="toothpick">
-      <img src={selected?"./remove.png":"./toothpick.png"}></img>
+      <div>
+        { firstRow }
+      </div>
+      <div>
+        
+      </div>
+      <div>
+        
+      </div>
+      
       
     </div>
   );
 }
 
-export default toothpick;
+export default panel;
